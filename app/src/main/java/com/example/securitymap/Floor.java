0d93eceba6a2e080307fa95655366f47bdffb7ac
GodJac <jgodi018@uottawa.ca>
2020-11-01 16:49:32 -0500

    LE PATH EST DRAWN SUR LES FLOOR PLANS!!

---
+++ app/src/main/java/com/example/securitymap/Floor.java
@@ -0,0 +1,10 @@
+package com.example.securitymap;
+
+public class Floor {
+    String name;
+    int num;
+    double width;
+    double height;
+    double ox;
+    double oy;
+}
--- app/src/main/java/com/example/securitymap/cby.java
+++ app/src/main/java/com/example/securitymap/cby.java
@@ -1,12 +1,16 @@
 package com.example.securitymap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
+
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -35,7 +39,8 @@ public class cby extends AppCompatActivity {
     private ArrayList<Build> buildNames;
     private ArrayList<Building> buildings;
     private ArrayList<Node> nodes;
-    private ArrayList<Bitmap> floorBitmaps = new ArrayList<Bitmap>();
+    private ArrayList<Bitmap> floorBitmaps = new ArrayList<>();
+    ArrayList<Integer> path = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,16 +53,21 @@ public class cby extends AppCompatActivity {
         floorPlan = (ImageView)findViewById(R.id.imageView2);
         mPosX = floorPlan.getX();
         mPosX = floorPlan.getY();
-        CSVFile csvFile = new CSVFile();
-        buildNames = csvFile.getBuildNames();
-        buildings = csvFile.getBuildings();
-        nodes = csvFile.getNodes();
+        buildNames = CSVFile.getBuildNames();
+        buildings = CSVFile.getBuildings();
+        nodes = CSVFile.getNodes();
+
+        for(Node node:nodes){
+            Log.d("nodes", node.toStr());
+        }
+
         Log.d("build", "allo");
         Log.d("build", build);
         Build b = Build.valueOf(build);
         building = buildings.get(buildNames.indexOf(b));
 
         //temporary:
+        /*
         building.floors = new ArrayList<String>();
         building.floors.add("Basement");
         building.floors.add("Floor 1 (Ground Level)");
@@ -65,24 +75,43 @@ public class cby extends AppCompatActivity {
         building.floors.add("Floor 3");
         building.floors.add("Floor 4");
         building.floors.add("Floor 5");
-
+        */
 
         SeekBar floor = (SeekBar) findViewById(R.id.seekBar2);
         floor.setMax(building.floors.size());
         final TextView floorText = (TextView)findViewById(R.id.textView4);
-        floorText.setText(building.floors.get(1));
+        floorText.setText(building.floors.get(1).name);
         final TextView buildingText = (TextView)findViewById(R.id.textView2);
         buildingText.setText(build);
         Paint myPaint = new Paint();
         myPaint.setColor(Color.RED);
         myPaint.setAntiAlias(true);
-        myPaint.setStrokeWidth(3);
+        myPaint.setStrokeWidth(5);
         myPaint.setStyle(Paint.Style.STROKE);
         myPaint.setStrokeJoin(Paint.Join.ROUND);
         myPaint.setStrokeCap(Paint.Cap.ROUND);
 
+        Dijkstra calculator = new Dijkstra();
+        calculator.calculatePath(nodes, 42, 52);
+        path = calculator.getPath();
+        Log.d("tag1", "\nPath:\n");
+        for(int i=0; i<path.size(); i++){
+            Log.d("tag1", String.valueOf(path.get(i)));
+        }
+
+        /*Drawable vectorDrawable = VectorDrawableCompat.create(getResources(), R.drawable.logo,  getContext().getTheme());
+        Bitmap myLogo = ((BitmapDrawable) vectorDrawable).getBitmap();
+
+        Drawable drawable = getResources().getDrawable(R.drawable.input);
+        Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.my_drawable);*/
+        //final BitmapFactory.Options mBitmapOptions = new BitmapFactory.Options();
+        //mBitmapOptions.inSampleSize = 4;
+        //mBitmapOptions.inJustDecodeBounds = true;
+        //BitmapFactory.decodeFile
         for(int i=0; i<building.floors.size(); i++){
-            String imageName = build.toString().toLowerCase()+i;
+            String imageName = build.toLowerCase()+i;
             int id = getResources().getIdentifier(imageName, "drawable",  getPackageName());
             floorPlan.setImageResource(id);
             Bitmap myBitmap = ((BitmapDrawable)floorPlan.getDrawable()).getBitmap();
@@ -90,16 +119,24 @@ public class cby extends AppCompatActivity {
             Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth()*2, myBitmap.getHeight()*2, Bitmap.Config.RGB_565);
             Canvas tempCanvas = new Canvas(tempBitmap);
             tempCanvas.drawBitmap(myBitmap, null, new Rect(0, 0, myBitmap.getWidth()*2, myBitmap.getHeight()*2), null);
-            //tempCanvas.drawLine(x1, y1, x2, y2, myPaint);
+            for (int k=0; k<path.size()-1; k++) {
+                if(nodes.get(path.get(k)).building == Build.valueOf(build)){
+                    if(nodes.get(path.get(k)).floor == nodes.get(path.get(k+1)).floor && nodes.get(path.get(k)).floor == i) {
+                        Floor tempFloor = building.floors.get(nodes.get(path.get(k)).floor);
+                        double ratio = myBitmap.getWidth()*2/tempFloor.width;
+                        tempCanvas.drawLine((int)(ratio*(nodes.get(path.get(k)).x + tempFloor.ox)), (int)(ratio*(tempFloor.height - nodes.get(path.get(k)).y - tempFloor.oy)), (int)(ratio*(nodes.get(path.get(k+1)).x + tempFloor.ox)), (int)(ratio*(tempFloor.height - nodes.get(path.get(k+1)).y - tempFloor.oy)), myPaint);
+                    }
+                }
+            }
             floorBitmaps.add(tempBitmap);
         }
-
+        floorPlan.setImageDrawable(new BitmapDrawable(getResources(), floorBitmaps.get(1)));
 
         floor.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar floor, int progress, boolean fromUser) {
                 floorPlan.setImageDrawable(new BitmapDrawable(getResources(), floorBitmaps.get(progress)));
-                floorText.setText(building.floors.get(progress));
+                floorText.setText(building.floors.get(progress).name);
             }
 
             @Override
